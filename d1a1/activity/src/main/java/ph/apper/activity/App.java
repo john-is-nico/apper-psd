5638a038e0a46b55f50c4323e43931b373bc4a1c
John Nicolas P. Montalvo <johnnicolas.montalvo@ad.mynt.xyz>
2021-04-06 14:03:35 +0800

    Activity 1

---
+++ d1a1/activity/src/main/java/ph/apper/activity/App.java
@@ -0,0 +1,38 @@
+package ph.apper.activity;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+
+    @RestController
+    @RequestParam("activity")
+    public static class ActivityController {
+
+        @PostMapping
+        public ResponseEntity create(@RequestBody Request request){
+            System.out.println(request);
+
+            return ResponseEntity.ok().build();
+        }
+
+
+    }
+
+    @Data
+    public static calss Request {
+        private String action;
+        private String Identifier;
+    }
+}
